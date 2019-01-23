1d7eba4df9fe96d4048fbf8f0a565483e9afbd4e
Bernard Lee <bernard.lee@nordicsemi.no>
2019-01-23 16:51:50 +0800

    Several LittlevGL tutorials.

---
+++ samples/adafruit/lcd/src/lv_tutorials.h
@@ -0,0 +1,35 @@
+/*
+ * Copyright (c) 2018 - 2019 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: LicenseRef-BSD-5-Clause-Nordic
+ */
+
+#ifndef LV_TUTORIALS_H__
+#define LV_TUTORIALS_H__
+
+/**
+ * Create a simple 'Hello world!' label
+ */
+void lv_tutorial_hello_world(void);
+
+/**
+ * Create some objects
+ */
+void lv_tutorial_objects(void);
+
+/**
+ * Create objects and styles
+ */
+void lv_tutorial_styles(void);
+
+/**
+ * Initialize a theme and create the same objects like "lv_tutorial_objects'
+ */
+void lv_tutorial_themes(void);
+
+/**
+ * Crate some objects an animate them
+ */
+void lv_tutorial_responsive(void);
+
+#endif
